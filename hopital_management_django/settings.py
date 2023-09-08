c71d4164e1022fd8ba2d2400a43f237da2d446e5
mahabubsaki <md929820@gmail.com>
2023-09-08 09:40:10 +0600

    static homepage done

--- hopital_management_django/settings.py
+++ hopital_management_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'personalize',
+    'authentications'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'hopital_management_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'common'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_URL = 'static/'
+# To use static files from the root
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ static/js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    const owl = $(".banner-carousal").owlCarousel({
+        items: 1,
+        loop: true,
+        lazyLoad: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+    });
+    $('#nextBtn').click(function () {
+        owl.trigger('next.owl.carousel');
+    });
+    $('#prevBtn').click(function () {
+        owl.trigger('prev.owl.carousel');
+    });
+    const owl2 = $(".team-carousal").owlCarousel({
+        items: 3,
+        loop: true,
+        lazyLoad: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+
+            700: {
+                items: 2,
+            },
+
+            1024: {
+                items: 3,
+            }
+        }
+    });
+    $('#nextBtn2').click(function () {
+        owl2.trigger('next.owl.carousel');
+    });
+    $('#prevBtn2').click(function () {
+        owl2.trigger('prev.owl.carousel');
+    });
+    $('#nextBtnv2').click(function () {
+        owl2.trigger('next.owl.carousel');
+    });
+    $('#prevBtnv2').click(function () {
+        owl2.trigger('prev.owl.carousel');
+    });
+});
+
