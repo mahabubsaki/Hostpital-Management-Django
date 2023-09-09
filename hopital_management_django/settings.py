f7fc0b1102ba62f36da617179a2a923cd1586beb
mahabubsaki <md929820@gmail.com>
2023-09-09 11:22:11 +0600

    d

--- hopital_management_django/settings.py
+++ hopital_management_django/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
